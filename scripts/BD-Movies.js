8cb351caeb164604b6d638d7cceae91a9a486608
lautaromdelgado <lautaromdelgado@gmail.com>
2024-06-23 03:27:11 -0300

    Renaming js file for database

---
+++ scripts/BD-Movies.js
@@ -0,0 +1,152 @@
+const movies = [
+  {
+    title: "Baby Driver",
+    image:
+      "https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/peliculas/baby-driver/127795123-4-esl-ES/Baby-Driver.jpg?resize=320:*",
+    description: "Acción, Crimen",
+  },
+  {
+    title: "El Padrino",
+    image:
+      "https://i.pinimg.com/564x/e9/b2/a2/e9b2a209513ab17132c4092bb4f462c0.jpg",
+    description: "Drama, Crimen",
+  },
+  {
+    title: "Pulp Fiction",
+    image:
+      "https://i.pinimg.com/564x/d1/47/bf/d147bf09730fc224539bd3383645acec.jpg",
+    description: "Crimen, Drama",
+  },
+  {
+    title: "La Lista de Schindler",
+    image:
+      "https://i.pinimg.com/564x/8b/64/83/8b648309a99984686519314731b56ea1.jpg",
+    description: "Drama, Historia, Bélica",
+  },
+  {
+    title: "El Caballero Oscuro",
+    image:
+      "https://i.pinimg.com/736x/a1/fb/b9/a1fbb9e9afb78013473689467b4456e7.jpg",
+    description: "Acción, Crimen, Drama",
+  },
+  {
+    title: "Forrest Gump",
+    image:
+      "https://i.pinimg.com/564x/2e/6a/18/2e6a18767bf8595aa20f62d79fd6743a.jpg",
+    description: "Drama, Romance",
+  },
+  {
+    title: "Titanic",
+    image:
+      "https://i.pinimg.com/564x/4d/6d/44/4d6d44a7e5ab575242fe5813821e4aa0.jpg",
+    description: "Drama, Romance",
+  },
+  {
+    title: "Matrix",
+    image:
+      "https://i.pinimg.com/564x/9e/85/34/9e8534c48e0168fb753057370627db8a.jpg",
+    description: "Acción, Ciencia Ficción",
+  },
+  {
+    title: "El Señor de los Anillos",
+    image:
+      "https://i.pinimg.com/564x/ff/0f/f9/ff0ff9b6bda8987b207f0b7ab38652fc.jpg",
+    description: "Aventura, Drama, Fantasía",
+  },
+  {
+    title: "Inception",
+    image:
+      "https://i.pinimg.com/564x/8d/c2/92/8dc29215da9026e341f105a157c4258a.jpg",
+    description: "Acción, Aventura, Ciencia Ficción",
+  },
+  {
+    title: "Gladiador",
+    image:
+      "https://i.pinimg.com/564x/73/7a/81/737a816f797b934694cc9dff393f68ff.jpg",
+    description: "Acción, Aventura, Drama",
+  },
+  {
+    title: "Ciudad de Dios",
+    image:
+      "https://i.pinimg.com/564x/46/d4/04/46d40401af2a84d7201de436ff9d1a81.jpg",
+    description: "Crimen, Drama",
+  },
+  {
+    title: "Star Wars",
+    image:
+      "https://i.pinimg.com/564x/c5/e8/e8/c5e8e8d495f384fcb8f3fe96ff479281.jpg",
+    description: "Acción, Aventura, Fantasía",
+  },
+  {
+    title: "El Silencio de los Inocentes",
+    image:
+      "https://i.pinimg.com/564x/b7/51/1c/b7511c9a0b7958c7857c9897b85900fb.jpg",
+    description: "Crimen, Drama, Suspenso",
+  },
+  {
+    title: "Volver al Futuro",
+    image:
+      "https://i.pinimg.com/564x/38/64/ad/3864adb100e4e33c6927a4027d3139ee.jpg",
+    description: "Aventura, Comedia, Ciencia Ficción",
+  },
+  {
+    title: "Fight Club",
+    image:
+      "https://i.pinimg.com/564x/05/98/d9/0598d97af0622e0c9a2b735f0bc44419.jpg",
+    description: "Drama",
+  },
+  {
+    title: "Jurassic Park",
+    image:
+      "https://i.pinimg.com/564x/9b/a3/5c/9ba35c1e0d966752b01206fd203a375f.jpg",
+    description: "Aventura, Ciencia Ficción, Suspenso",
+  },
+  {
+    title: "La La Land",
+    image:
+      "https://i.pinimg.com/564x/18/53/21/1853212adb0f124f2232a2e7058c418d.jpg",
+    description: "Comedia, Drama, Musical",
+  },
+  {
+    title: "Parásitos",
+    image:
+      "https://i.pinimg.com/564x/53/35/80/533580942180b3043049480c294cb5d8.jpg",
+    description: "Comedia, Drama, Suspenso",
+  },
+  {
+    title: "Furia en el Camino",
+    image:
+      "https://i.pinimg.com/564x/2b/be/a6/2bbea69b9b1acb7d6f9e71c5748a6625.jpg",
+    description: "Acción, Aventura, Ciencia Ficción",
+  },
+  {
+    title: "Toy Story 3",
+    image:
+      "https://i.pinimg.com/564x/66/a7/e5/66a7e59fc6d2b1e5c2998ed0c57d21fa.jpg",
+    description: "Animación, Aventura, Comedia",
+  },
+  {
+    title: "La Vida es Bella",
+    image:
+      "https://i.pinimg.com/564x/43/0e/55/430e55492caf51608ad749c9a72a62ba.jpg",
+    description: "Comedia, Drama, Romance",
+  },
+  {
+    title: "El Gran Hotel Budapest",
+    image:
+      "https://i.pinimg.com/564x/c5/78/bf/c578bfb3862d2b35c84aa831fba6daa6.jpg",
+    description: "Aventura, Comedia, Crimen",
+  },
+  {
+    title: "Alien: El Octavo Pasajero",
+    image:
+      "https://i.pinimg.com/564x/f4/62/13/f462133e0f471730af9dbdd1a024a7cb.jpg",
+    description: "Horror, Ciencia Ficción",
+  },
+  {
+    title: "El Rey León",
+    image:
+      "https://i.pinimg.com/564x/09/49/18/094918fe5f68d7af592200e0dec14db8.jpg",
+    description: "Animación, Aventura, Drama",
+  },
+];
