8fae758d37907e2e1b68eb4dc57416d35f6c23dd
lautaromdelgado <lautaromdelgado@gmail.com>
2024-06-25 15:14:43 -0300

    Modification of the home in index.html.

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const moviesContainer = document.getElementById("movies-container");
-
-  movies.forEach((movie) => {
-    const movieElement = document.createElement("div");
-    movieElement.classList.add("movie");
-
-    movieElement.innerHTML = `
-            <img src="${movie.image}" alt="${movie.title}">
-            <h2>${movie.title}</h2>
-            <p>${movie.description}</p>`;
-
-    moviesContainer.appendChild(movieElement);
-  });
-
-  //Funcionalidad para abrir el form de contacto en una nueva pestaña
-  const contactButton = document.getElementById("contactButton");
+// Funcionalidad para abrir el form de contacto en una nueva pestaña
+const contactButton = document.getElementById("contactButton");
+if (contactButton) {
   contactButton.addEventListener("click", (e) => {
+    e.preventDefault();
     window.location.href = "form.html";
   });
-  //Funcionalidad "volver atrás" con elbotón del formulario.
-  const volverAtras = document.getElementById("btn-volver-atras");
+}
+
+// Funcionalidad "volver atrás" con el botón del formulario
+const volverAtras = document.getElementById("btn-volver-atras");
+if (volverAtras) {
   volverAtras.addEventListener("click", (e) => {
+    e.preventDefault();
     window.location.href = "index.html";
   });
-});
+}
