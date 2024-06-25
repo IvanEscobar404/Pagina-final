c427fc3277fe70c518a966ddd96d165556a99713
lautaromdelgado <lautaromdelgado@gmail.com>
2024-06-25 20:08:28 -0300

    Connection to the contact form, from the 'everything' category.

--- scripts/category-main.js
+++ scripts/category-main.js
@@ -6,3 +6,11 @@ if (homeButton) {
     window.location.href = "../index.html";
   });
 }
+
+const contactoButton = document.getElementById("contacto-boton");
+if (contactoButton) {
+  contactoButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "../form.html";
+  });
+}
