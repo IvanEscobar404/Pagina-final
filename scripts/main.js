9172c19db700eca779727d54a134c34c8a31efff
lautaromdelgado <lautaromdelgado@gmail.com>
2024-06-25 15:48:43 -0300

    Adding functionality to navigate between categories.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-// Funcionalidad para abrir el form de contacto en una nueva pestaña
+// Funcionalidad para abrir el form de contacto
 const contactButton = document.getElementById("contactButton");
 if (contactButton) {
   contactButton.addEventListener("click", (e) => {
@@ -13,5 +13,50 @@ if (volverAtras) {
   volverAtras.addEventListener("click", (e) => {
     e.preventDefault();
     window.location.href = "index.html";
+  });
+}
+
+// Funcionalidad para abrir "todo" en categoría
+const todoButton = document.getElementById("category-todo");
+if (todoButton) {
+  todoButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/category/todo.html";
+  });
+}
+
+// Funcionalidad para abrir "acción" en categoría
+const accionButton = document.getElementById("category-accion");
+if (accionButton) {
+  accionButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/category/accion.html";
+  });
+}
+
+// Funcionalidad para abrir "comedia" en categoría
+const comediaButton = document.getElementById("category-comedia");
+if (comediaButton) {
+  comediaButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/category/comedia.html";
+  });
+}
+
+// Funcionalidad para abrir "drama" en categoría
+const dramaButton = document.getElementById("category-drama");
+if (dramaButton) {
+  dramaButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/category/drama.html";
+  });
+}
+
+// Funcionalidad para abrir "romance" en categoría
+const romanceButton = document.getElementById("category-romance");
+if (romanceButton) {
+  romanceButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/category/romance.html";
   });
 }
