c6b5cb3993387e9080994c18c326cdb875f293b5
lautaromdelgado <lautaromdelgado@gmail.com>
2024-06-25 20:48:50 -0300

    code optimization in category-main.

--- scripts/category-main.js
+++ scripts/category-main.js
@@ -2,15 +2,16 @@
 const homeButton = document.getElementById("home-boton");
 if (homeButton) {
   homeButton.addEventListener("click", (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Evitar que js haga una funcionalidad por default
     window.location.href = "../index.html";
   });
 }
 
+// Funcionalidad para abrir "Contacto" el formulario.
 const contactoButton = document.getElementById("contacto-boton");
 if (contactoButton) {
   contactoButton.addEventListener("click", (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Evitar que js haga una funcionalidad por default
     window.location.href = "../form.html";
   });
 }
