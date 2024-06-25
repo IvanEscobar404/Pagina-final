0ca1dc48cdfa70bc72dc8ccefe9c3a1b19beba44
lautaromdelgado <lautaromdelgado@gmail.com>
2024-06-25 15:49:59 -0300

    Added filter to put all movies in the everything category.

---
+++ scripts/Filtro.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const moviesContainer = document.getElementById("movies-container");
+
+  movies.forEach((movie) => {
+    const movieElement = document.createElement("div");
+    movieElement.classList.add("movie");
+
+    movieElement.innerHTML = `
+              <img src="${movie.image}" alt="${movie.title}">
+              <h2>${movie.title}</h2>
+              <p>${movie.description}</p>`;
+
+    moviesContainer.appendChild(movieElement);
+  });
+
+  //Funcionalidad para abrir el form de contacto en una nueva pestaña
+  const contactButton = document.getElementById("contactButton");
+  contactButton.addEventListener("click", (e) => {
+    window.location.href = "form.html";
+  });
+  //Funcionalidad "volver atrás" con elbotón del formulario.
+  const volverAtras = document.getElementById("btn-volver-atras");
+  volverAtras.addEventListener("click", (e) => {
+    window.location.href = "index.html";
+  });
+});
