e1ceac582376a5766ac55ab1151fa2114d14f9d3
lautaromdelgado <lautaromdelgado@gmail.com>
2024-06-27 15:26:24 -0300

    Solution to the error that was not directed to the main menu from all categories.

--- scripts/category/drama.js
+++ scripts/category/drama.js
@@ -15,3 +15,77 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Funcionalidades para usar los botones de la barra de navegación
+
+// Funcionalidad para abrir el form de contacto
+const contactButton = document.getElementById("contactButton");
+if (contactButton) {
+  contactButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "../../form.html";
+  });
+}
+
+// Funcionalidad "volver atrás" con el botón del formulario
+const volverAtras = document.getElementById("btn-volver-atras");
+if (volverAtras) {
+  volverAtras.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "../../index.html";
+  });
+}
+
+// Funcionalidad para abrir "Home" en categoría
+const homeButton = document.getElementById("home-boton");
+if (homeButton) {
+  homeButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "./../index.html";
+  });
+}
+
+// Funcionalidad para abrir "todo" en categoría
+const todoButton = document.getElementById("category-todo");
+if (todoButton) {
+  todoButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/category/todo.html";
+  });
+}
+
+// Funcionalidad para abrir "acción" en categoría
+const accionButton = document.getElementById("category-accion");
+if (accionButton) {
+  accionButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/category/accion.html";
+  });
+}
+
+// Funcionalidad para abrir "comedia" en categoría
+const comediaButton = document.getElementById("category-comedia");
+if (comediaButton) {
+  comediaButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/category/comedia.html";
+  });
+}
+
+// Funcionalidad para abrir "drama" en categoría
+const dramaButton = document.getElementById("category-drama");
+if (dramaButton) {
+  dramaButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/category/drama.html";
+  });
+}
+
+// Funcionalidad para abrir "romance" en categoría
+const romanceButton = document.getElementById("category-romance");
+if (romanceButton) {
+  romanceButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/category/romance.html";
+  });
+}
